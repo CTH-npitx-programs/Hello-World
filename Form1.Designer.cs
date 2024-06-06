ce3c3e561cc87991f6c7a0257f0c3958547ccad0
CTH-npitx <chatch@npitx.org>
2024-06-06 14:52:57 -0500

    seperate text boxes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -78,7 +78,7 @@
             // txt_output
             // 
             this.txt_output.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.txt_output.Location = new System.Drawing.Point(170, 12);
+            this.txt_output.Location = new System.Drawing.Point(170, 91);
             this.txt_output.MaxLength = 20;
             this.txt_output.Name = "txt_output";
             this.txt_output.Size = new System.Drawing.Size(407, 44);
