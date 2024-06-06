4bb61e4f7d1aeb9eea326dff00f8adbe1f18c524
CTH-npitx <chatch@npitx.org>
2024-06-06 11:50:21 -0500

    commited the original... don't ask, things are behaveing weird

--- Form1.cs
+++ Form1.cs
@@ -25,34 +25,14 @@ namespace Hello_World
             //this.Close();
             //only closes whole thing if used on parent form
             Application.Exit();
-  
+
         }
 
         private void btn_go_Click(object sender, EventArgs e)
         {
-            var greeting = "Hello " + txt_name.Text;
+            var greeting = "Hello World!";
             txt_output.Text = greeting;
 
-        }
-
-        private void txt_name_TextChanged(object sender, EventArgs e)
-        {
-            if (txt_name.Text.Length() > 0)
-            {
-                bttn_nameIn.Hide();
-            }
-            {
-                bttn_nameIn.Show();
-            }
-            // if kept as "greater than 0", invert, as hide should only be if less than 0
-        }
-
-        private void bttn_nameIn_Click(object sender, EventArgs e)
-        {
-            btn_go.Show();
-            txt_output.Show();
-            bttn_nameIn.Hide();
-            txt_name.Hide();
         }
     }
 }
